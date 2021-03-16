392a1aa14643de8b4496558b0542a6fbd0573d91
taylor200-tlr <taylor200@freemail.hu>
2021-03-16 20:22:28 +0100

    vizsga 3. feladat (a szokásos problémámmal: nincs meg a file) :(

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/Employee.java
@@ -0,0 +1,27 @@
+package hu.nive.ujratervezes.zarovizsga;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+    private int workedHours;
+    private LocalDate workDay;
+
+    public Employee(String name, int workedHours, LocalDate workDay) {
+        this.name = name;
+        this.workedHours = workedHours;
+        this.workDay = workDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWorkedHours() {
+        return workedHours;
+    }
+
+    public LocalDate getWorkDay() {
+        return workDay;
+    }
+}
---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/WorkHours.java
@@ -0,0 +1,65 @@
+package hu.nive.ujratervezes.zarovizsga;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkHours {
+    private List<Employee> employees = new ArrayList<>();
+
+    public String minWork(String file){
+        getPathFromFile(file);
+        Path path = Path.of(file);
+        readLine(path);
+        String result = getMinWorkedEmployee().getName() + ": " + getMinWorkedEmployee().getWorkDay();
+        return result;
+    }
+
+    private Employee getMinWorkedEmployee() {
+        int minValue = Integer.MAX_VALUE;
+        Employee result = null;
+        for (Employee employee:employees){
+            if(employee.getWorkedHours()< minValue){
+                result = employee;
+            }
+        }
+        return result;
+    }
+
+    private void getPathFromFile(String file) {
+        // Itt kellene megcsinálnom a Path.of paraméterezését a file eléréséhez
+        // pl file splittelése "/" regex mentén
+        // ha szükséges ilyen
+        // de akkor nem void lenne, hanem path-t adna vissza
+        // és azzal lenne meghívva a readline
+    }
+
+    private void readLine(Path path){
+        String line;
+        try(BufferedReader reader = Files.newBufferedReader(path)){
+            while ((line = reader.readLine()) != null ){
+                createEmployeesFromLine(line);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file!", ioe);
+        }
+    }
+
+    private void createEmployeesFromLine(String line) {
+        String[] data = line.split(",");
+        String[] date = data[2].split("-");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        employees.add(new Employee(data[0], Integer.parseInt(data[1]), LocalDate.of(year, month, day)));
+    }
+
+    public static void main(String[] args) {
+        WorkHours workHours = new WorkHours();
+        workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+    }
+}
