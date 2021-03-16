2c88c31bf4c296ca83ad5a3a99bf629e0aa18523
taylor200-tlr <taylor200@freemail.hu>
2021-03-16 18:20:42 +0100

    vizsga

--- src/main/java/hu/nive/ujratervezes/zarovizsga/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/DigitsCounter.java
@@ -1,12 +1,24 @@
 package hu.nive.ujratervezes.zarovizsga;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DigitsCounter {
-    public int getCountOfDigits(String s){
+    public int getCountOfDigits(String s) {
+        List<Character> characters = new ArrayList<>();
         int result = 0;
-        if ((s == null) || (s.length()<1)){
+        if ((s == null) || (s.length() < 1)) {
             return result;
         }
+        for (int i = 0; i < s.length(); i++) {
 
-        return result;
+            if(Character.isDigit(s.charAt(i))){
+                if (!characters.contains(s.charAt(i))){
+                    characters.add(s.charAt(i));
+                }
+            }
+
+        }
+        return characters.size();
     }
 }
