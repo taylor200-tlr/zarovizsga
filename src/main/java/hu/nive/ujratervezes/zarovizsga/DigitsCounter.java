93854cf2ff489194da4b9bb4307d81bf255c5689
taylor200-tlr <taylor200@freemail.hu>
2021-03-16 18:03:55 +0100

    vizsga

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/DigitsCounter.java
@@ -0,0 +1,12 @@
+package hu.nive.ujratervezes.zarovizsga;
+
+public class DigitsCounter {
+    public int getCountOfDigits(String s){
+        int result = 0;
+        if ((s == null) || (s.length()<1)){
+            return result;
+        }
+
+        return result;
+    }
+}
